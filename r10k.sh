c5f6a86a6e1ee2513e15db3083636a5af1e4253c
Kris Lamoureux <krislamo@uga.edu>
2019-10-02 14:57:44 -0400

    Quick temporary fix for broken hiera (rm#38378)

--- r10k.sh
+++ r10k.sh
@@ -18,3 +18,6 @@ if ! grep -q "export PATH=" /root/.bashrc; then
   echo "export PATH=\$PATH:/usr/local/bin" >> /root/.bashrc
 fi
 
+# Temporary fix for broken hiera on vm
+GLOBAL_HIERA=/etc/puppetlabs/puppet/hiera.yaml
+sed -i '/  - common/{N; /\n  - users$/b; s/\n/\n  - users\n/}' $GLOBAL_HIERA
